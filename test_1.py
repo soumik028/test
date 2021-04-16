18ce2b0f48c0bead8220d0fb7c3ce3429698c1bd
soumik028 <59434308+soumik028@users.noreply.github.com>
2021-04-16 20:27:21 +0530

    Update test_1.py

--- test_1.py
+++ test_1.py
@@ -5,13 +5,6 @@ Created on Fri Apr 16 16:12:07 2021
 @author: dasso
 """
 import time
-import win32gui, win32con
-
-time.sleep(2)
-Minimize = win32gui.GetForegroundWindow()
-win32gui.ShowWindow(Minimize, win32con.SW_MINIMIZE)
-
-
 
 import ctypes
 user32 = ctypes.windll.user32
