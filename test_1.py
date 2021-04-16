116e6c04263250d9b909d7c20135f18499679c96
soumik028 <59434308+soumik028@users.noreply.github.com>
2021-04-16 20:33:32 +0530

    Update test_1.py

--- test_1.py
+++ test_1.py
@@ -4,9 +4,9 @@ Created on Fri Apr 16 16:12:07 2021
 
 @author: dasso
 """
-import time
+import tkinter as tk
 
-import ctypes
-user32 = ctypes.windll.user32
-screensize = user32.GetSystemMetrics(0), user32.GetSystemMetrics(1)
-st.write(screensize)
+root = tk.Tk()
+
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
